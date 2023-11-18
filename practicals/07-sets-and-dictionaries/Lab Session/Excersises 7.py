d2d65a1c1d4df20dfff76061bc4a518d29e99da5
Danny Liles <dannyspam404@gmail.com>
2023-11-18 21:00:26 +0000

    Week 7 excersises and practical finished. Documents uploaded.

---
+++ practicals/07-sets-and-dictionaries/Lab Session/Excersises 7.py
@@ -0,0 +1,14 @@
+languages = set(["C++", "Java", "C#", "PHP", "JavaScript"])
+
+print(languages)
+
+vowels = set("aeiou")
+print(vowels)
+
+names = set("John", "Eric", "Terry", "Michael", "Graham", "Terry")
+print(names)
+
+chars = {chr(n) for n in range(32, 128)}
+
+lang_gen = { "Java":3, "Assembly":2, "Machine Code":1 }
+lang_gen = dict(["Java":3, "Assembly":2, "Machine Code":1]s)
