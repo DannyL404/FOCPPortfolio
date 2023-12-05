e024e3dec7a35447bea31b129dfeda6e4e2a3f09
Danny Liles <dannyspam404@gmail.com>
2023-12-05 13:15:51 +0000

    All input is gathered

--- assignment/Task1/pizza_price.py
+++ assignment/Task1/pizza_price.py
@@ -8,35 +8,41 @@ if __name__ == '__main__':
 #These are the variables for the pizza shop. They are the key facts that will be determined by user input.
 Pizza_cost = 12
 Pizza_number = 0
-Pizza_Type = []
 Tuesday = False
 App_Used = False
+Delivery_Required = False
 Data_Valid = 0
 
 #This is the information that is needed to be gathered from the user
 
+#Checks the number of Pizzas Ordered
+Pizza_number = input("Please enter the number of Pizzas ordered")
+
 #Checking if the day is Tuesday
-Tuesday = input("""Is today Tuesday? \n Please enter 'True' or 'False'""")
-while Data_Valid == 0:
-    if Tuesday == "True":
-        Tuesday = bool(Tuesday)
-        Data_Valid += 1
-    elif Tuesday == "False":
-        Tuesday = bool(Tuesday)
-        Data_Valid += 1
-    else:
-        Tuesday = input("""Is today Tuesday? \n Please enter 'True' or 'False'""")
+day = input("Enter the day of the week")
+day = day.lower()
+if day == "tuesday":
+    Tuesday = True
+else:
+    Tuesday = False
 
-#Checking if the app was used.
-App_Used = input("""Is today Tuesday? \n Please enter 'True' or 'False'""")
-while Data_Valid == 0:
-    if App_Used == "True":
-        App_Used = bool(App_Used)
-        Data_Valid += 1
-    elif App_Used == "False":
-        App_Used = bool(App_Used)
-        Data_Valid += 1
-    else:
-        App_Used = input("""Is today Tuesday? \n Please enter 'True' or 'False'""")
+#This checks if delivery is required
+Delivery_Check = input("""Is delivery required (enter as yes / no)""")
+Delivery_Check = Delivery_Check.lower()
+if Delivery_Check == "yes":
+    Delivery_Required = True
+else:
+    Delivery_Required = False
 
-print(type(Tuesday))
+#This checks if the app was used
+App_Check = input("""Was the app used (enter as yes / no)""")
+App_Check = App_Check.lower()
+if App_Check == "yes":
+    App_Used = True
+else:
+    App_Used = False
+
+
+
+
+
