d0c1f02261574632a46e30bbb658d985e1763ca9
Dan L <d.liles3266@student.leedsbeckett.ac.uk>
2023-12-18 21:22:34 +0000

    creating a chunk of data as a variable from the file.
    also starting an IF statement to calculate the result.

--- assignment/Task2/cat_shelter.py
+++ assignment/Task2/cat_shelter.py
@@ -13,7 +13,16 @@ attack_theirs = 0
 time_ours_in = 0
 Longest_visit = 0
 shortest_visit = 0
+total_visit_time = 0
 
 for line in lines:
-    line = line.strip('END'):
-    break
+    if line == line.strip('END'):
+        break
+
+information = cat_type, entry_time, exit_time
+entry_time = int(entry_time)
+exit_time = int(exit_time)
+
+if ours_enters == True:
+    ours_enters += 1
+  #  total_visit_time =
