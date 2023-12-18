419200e37e2d6c8b3dbe46345392137f041e9609
Dan L <d.liles3266@student.leedsbeckett.ac.uk>
2023-12-18 21:11:41 +0000

    defined a function that reads the file

--- assignment/Task2/cat_shelter.py
+++ assignment/Task2/cat_shelter.py
@@ -1,6 +1,13 @@
 if __name__ == '__main__':
     import sys
 
+def shelter_analysis(log_file_path):
+    try:
+        with open(log_file_path, 'r') as file:
+            lines = file
+    except:
+        print("File not found")
+
 ours_enters = 0
 attack_theirs = 0
 time_ours_in = 0
