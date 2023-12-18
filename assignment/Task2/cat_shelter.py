dde8288b9d9184e9e800c97138fcd1ff45aee59f
Dan L <d.liles3266@student.leedsbeckett.ac.uk>
2023-12-18 21:48:28 +0000

    commented my code to make it easier for me to find stuff.

--- assignment/Task2/cat_shelter.py
+++ assignment/Task2/cat_shelter.py
@@ -1,5 +1,6 @@
 import sys
 
+#function to read the log file from the command line argument
 def shelter_analysis(log_file_path):
     try:
         with open(log_file_path, 'r') as file:
@@ -7,29 +8,42 @@ def shelter_analysis(log_file_path):
     except:
         print("File not found")
 
+#key variables
 ours_enters = 0
 attack_theirs = 0
 time_ours_in = 0
-Longest_visit = 0
+longest_visit = 0
 shortest_visit = 0
 total_visit_time = 0
 
+#stops the program if reading the line END
 for line in lines:
     if line == line.strip('END'):
         break
 
+#Defines variables needed for the next if statement from the log file
 information = cat_type, entry_time, exit_time
 entry_time = int(entry_time)
 exit_time = int(exit_time)
 
+#Runs checks against the log file to update variables for final output.
 if ours_enters == True:
     ours_enters += 1
     total_visit_time = exit_time - entry_time
     time_ours_in += total_visit_time
 
+if total_visit_time > longest_visit:
+    total_visit_time = longest_visit
+
+if total_visit_time < shortest_visit:
+    total_visit_time = shortest_visit
+
 elif attack_theirs == True:
     attack_theirs += 1
 
+#Final program output
 if ours_enters == 0:
     print("The cat did not enter today")
+
+else:
 
