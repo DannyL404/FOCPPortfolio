b455be9b5e0bff0ab76a89854fa5843af6fd34e8
Danny Liles <dannyspam404@gmail.com>
2023-12-21 11:24:22 +0000

    Fixed the cat shelter program. Done by redefining variables and calculations. Also fixed the function in the program file as the else statement was indented incorectly.

--- assignment/Task2/cat_shelter.py
+++ assignment/Task2/cat_shelter.py
@@ -1,56 +1,57 @@
 import sys
 
-#function to read the log file from the command line argument
+
+# function to read the log file from the command line argument
 def shelter_analysis(log_file_path):
     try:
         with open(log_file_path, 'r') as file:
-           lines = file.readlines()
+            lines = file.readlines()
     except:
         print("File not found")
 
-# key variables
-ours_enters = 0
-attack_theirs = 0
-time_ours_in = 0
-longest_visit = 0
-shortest_visit = 0
-total_visit_time = 0
+#key variables
+    ours_enters = 0
+    theirs_attacks = 0
+    time_ours_in_house = 0
+    longest_visit = 0
+    shortest_visit = 9999999999999
+    total_visit_time = 0
 
-# stops the program if reading the line END
-for line in lines:
-    if line == line.strip('END'):
-        break
+    # stops the program if reading the line END
+    for line in lines:
+        if line == ('END'):
+            break
 
-# Defines variables needed for the next if statement from the log file
-information = cat_type, entry_time, exit_time
-entry_time = int(entry_time)
-exit_time = int(exit_time)
+        # Defines variables needed for the next if statement from the log file
+        information = line.split(',')
+        cat_type = information[0]
+        entry_time = int(information[1])
+        exit_time = int(information[2])
 
-# Runs checks against the log file to update variables for final output.
-if ours_enters == True:
-    ours_enters += 1
-    total_visit_time = exit_time - entry_time
-    time_ours_in += total_visit_time
+        # Runs checks against the log file to update variables for final output.
+        if cat_type == 'OURS':
+            ours_enters += 1
+            total_visit_time = exit_time - entry_time
+            time_ours_in_house += total_visit_time
 
-if total_visit_time > longest_visit:
-    total_visit_time = longest_visit
+            if total_visit_time > longest_visit:
+                longest_visit = total_visit_time
 
-if total_visit_time < shortest_visit:
-    total_visit_time = shortest_visit
+            if total_visit_time < shortest_visit:
+                shortest_visit = total_visit_time
 
-elif attack_theirs == True:
-    attack_theirs += 1
+        elif cat_type == 'THEIRS':
+            theirs_attacks += 1
 
-average_visit_time = total_visit_time / ours_enters
+    average_visit_time = time_ours_in_house / ours_enters
 
-# Final program output
-if ours_enters == 0:
-    print("The cat did not enter today")
+    # Final program output
+    print(f"Total number of times OUR cat entered the house: {ours_enters}")
+    print(f"Number of times unwanted cats entered and were attacked: {theirs_attacks}")
+    print(f"Total time spent in the house by OUR cat: {time_ours_in_house // 60} hours, {time_ours_in_house % 60} minutes")
+    print(f"Average duration of each visit for OUR cat: {average_visit_time:.2f} minutes")
+    print(f"Longest visit duration of OUR cat: {longest_visit} minutes")
+    print(f"Shortest visit duration of OUR cat: {shortest_visit} minutes")
 
-else:
-    print(f"OUR cat entered {ours_enters} times.")
-    print(f"Other cats entered and were attacked {attack_theirs} times.")
-    print(f"OUR cat spent {time_ours_in} minutes in the house.")
-    print(f"OUR cat was in the house for an everage of {average_visit_time} minutes.")
-    print(f"The longest of these visits was {longest_visit}.")
-    print(f"The shortest of these visits was {shortest_visit}.")
+
+
--- assignment/Task2/cat_shelter_program.py
+++ assignment/Task2/cat_shelter_program.py
@@ -4,7 +4,7 @@ import cat_shelter
 if __name__ == "__main__":
     if len(sys.argv) > 2:
         print('There is no file selected!')
-else:
-    cat_shelter.shelter_analysis(sys.argv[1])
+    else:
+        cat_shelter.shelter_analysis(sys.argv[1])
 
 
