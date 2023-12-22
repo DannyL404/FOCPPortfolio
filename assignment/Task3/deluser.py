11eba60af708598a9518156465c2bf6d0884bcbe
Danny Liles <dannyspam404@gmail.com>
2023-12-22 14:27:35 +0000

    The program now works as intended. The passwords file is missing George as a username as I used his name to test.

--- assignment/Task3/deluser.py
+++ assignment/Task3/deluser.py
@@ -2,24 +2,24 @@ import sys
 
 def deluser_program(password_file_path):
     try:
-        with open(password_file_path, 'r+') as file:
+        with open(password_file_path, 'r') as file:
             lines = file.readlines()
             selected_username = input("Please enter the username you would like to delete")
-            for line_num, line in enumerate(lines, 1):
-                if selected_username in line:
-                    print("Match found!")
-                    return selected_username
-            print("No username found")
-            return None
-    except:
+            included_lines = [line for line in lines if selected_username not in line]
+
+        with open(password_file_path, 'w') as file:
+            file.writelines(included_lines)
+
+        if len(included_lines) < len(lines):
+            print(f"{selected_username} has been successfully deleted.")
+        else:
+            print("No match found!")
+
+    except FileNotFoundError:
         print("File not found")
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print('There is not file selected!')
+        print('There is no file selected!')
     else:
-        username_match = deluser_program(sys.argv[1])
-        if username_match is not None:
-            print(username_match)
-        else:
-            print("No username matched.")
+        deluser_program(sys.argv[1])
